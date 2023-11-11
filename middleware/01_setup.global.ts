bf5d41c96ee727b2343b21873c7d66f07eb7b2b5
Hady DIALLO <hdiallo89@gmail.com>
2023-11-11 11:55:46 +0100

    8. Error Handling

--- middleware/01_setup.global.ts
+++ middleware/01_setup.global.ts
@@ -1,3 +1,26 @@
-export default defineNuxtRouteMiddleware((to, from) => {
+export default defineNuxtRouteMiddleware(async (to, from) => {
   console.log("--- setup");
+
+  /*   if (to.path === "/login") {
+    throw createError({
+      statusCode: 405,
+      message: "Ops something wrong ! ",
+    });
+  } */
+
+  /*   abortNavigation({
+    statusCode: 405,
+    message: "Ops something wrong ! ",
+  }); */
+
+  /*  try {
+    const data = await fetch("/api/user");
+    return navigateTo(`/profile/${data}`);
+  } catch (error) {
+    if (error instanceof Error || typeof error === "string") {
+      abortNavigation(error);
+    } else {
+      abortNavigation("Ops something when loading user data");
+    }
+  } */
 });
